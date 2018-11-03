e2c89beaf4363179dcede77036f8dfdbcba6847f
Vladislav <denicovvlad_i@mail.ru>
2018-11-03 18:41:19 +0200

    initial commit

---
+++ Homework1/Homework1.js
@@ -0,0 +1,53 @@
+//-------------------------------------------------Task1
+var a = 2, b = 1;
+function checkNumber(a,b) {
+    if(a > b) {
+       return true;
+    }
+    else
+       return false;
+}
+checkNumber(a,b);
+
+//-------------------------------------------------Task2
+
+var a = 'RARE';
+function addString(a) {
+       return "You are input: " + a  ;
+}
+addString(a);
+
+//-------------------------------------------------Task3
+
+var f;
+function checkType(f){
+if (f === null){
+return null;
+}
+if (f === undefined) {
+return undefined;
+}
+else return false;
+}
+checkType(f);
+
+//-------------------------------------------------Task4
+
+var Obj = {};
+function checkObj(Obj){
+Obj.checked = true;
+console.log(Obj.checked);
+}
+checkObj(Obj);
+
+//-------------------------------------------------Task5
+
+var a = 20;
+function allNumbers(a){
+var c = "0";
+for(var i = 1; i <= a; i++) {
+  c += i;
+}
+console.log(c);
+}
+allNumbers(a);
